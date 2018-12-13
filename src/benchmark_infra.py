46889dddfc11a17d18793cc2e960cbccc40294ab
Hyungro Lee <hroe.lee@gmail.com>
2018-12-13 08:26:26 +0000

    automated script generator added

---
+++ src/benchmark_infra.py
@@ -0,0 +1,40 @@
+import json
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--file", help="json to import resource infomation", default="benchmark_resources.json")
+args = parser.parse_args()
+
+def gen_vars(data, prefix):
+	msg = []
+	for k,v in data.items():
+		msg.append ("export {0}{1}={2}".format(prefix, k, v))
+
+	return "\n".join(msg)
+
+with open(args.file) as f:
+	data = json.load(f)
+	for k1, v1 in data.items():
+		print ("source env-vars")
+		for k2, v2 in v1.items():
+			if k2 == "TF_VAR":
+				print gen_vars(v2, "TF_VAR_")
+
+			elif k2 == "_block":
+				print ("if [ -L \"block.tf\" ]; then rm block.tf; fi")
+				if 'linked_filename' in v2:
+					print ("ln -s {0} block.tf".format(v2['linked_filename']))
+					print ("sed -i -e 's/enable_data_tiering=\"0\"/enable_data_tiering=\"1\"/' ../scripts/disk_setup.sh")
+				else:
+					print ("sed -i -e 's/enable_data_tiering=\"1\"/enable_data_tiering=\"0\"/' ../scripts/disk_setup.sh")
+		print ("env|grep TF")
+		print ("echo \"terraform apply======================\"")
+		print ("terraform init;terraform plan;terraform apply -auto-approve\n")
+		print ("mkdir {0}".format(k1))
+		print ("cp terraform.tfstate {0}/".format(k1))
+		print ("python benchmark_trigger.py --title {0} > {0}/trigger.sh".format(k1))
+		print ("bash {0}/trigger.sh 2>> {0}/error.log 1> {0}/trigger.log".format(k1))
+		print ("while [ 1 ]; do if [ -e \"{0}/results.tar.gz\" ] ; then break; else sleep 60; fi; done".format(k1))
+		print ("echo \"terraform destroy====================\"")
+		print ("terraform destroy -auto-approve >> {0}/terraform.log".format(k1))
+
---
+++ src/benchmark_trigger.py
@@ -0,0 +1,20 @@
+import json
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--title")
+args = parser.parse_args()
+
+with open("terraform.tfstate") as f:
+        jdata = json.load(f)
+        cdh_utility_1 = (jdata['modules'][0]['resources']["data.oci_core_vnic.utility_node_vnic"]['primary']['attributes']['public_ip_address'])
+        cdh_bastion_1 = (jdata['modules'][0]['resources']["data.oci_core_vnic.bastion_vnic"]['primary']['attributes']['public_ip_address'])
+	print ("while [ 1 ]; do is_bastion_running=`ssh -o StrictHostKeyChecking=no opc@{0} \"ps -ef|grep bastion.sh |grep -v grep|wc -l\"`; if [ 0 -eq $is_bastion_running ]; then break; else sleep 60; fi;done".format(cdh_bastion_1))
+	print ("ssh -o StrictHostKeyChecking=no root@{0} yum install -y git".format(cdh_utility_1))
+	print ("ssh -o StrictHostKeyChecking=no root@{0} mkdir /var/lib/hadoop-hdfs/.ssh".format(cdh_utility_1))
+	print ("ssh -o StrictHostKeyChecking=no root@{0} cp .ssh/authorized_keys /var/lib/hadoop-hdfs/.ssh".format(cdh_utility_1))
+	print ("ssh -o StrictHostKeyChecking=no root@{0} chown -R hdfs: /var/lib/hadoop-hdfs/.ssh".format(cdh_utility_1))
+	print ("scp -o StrictHostKeyChecking=no benchmark_trigger_cmd.txt hdfs@{0}:.".format(cdh_utility_1))
+	print ("ssh -o BatchMode=yes -o StrictHostKeyChecking=no -o ConnectTimeout=7200 hdfs@{0} bash benchmark_trigger_cmd.txt".format(cdh_utility_1))
+	print ("scp hdfs@{0}:results.tar.gz {1}/".format(cdh_utility_1, args.title))
+
